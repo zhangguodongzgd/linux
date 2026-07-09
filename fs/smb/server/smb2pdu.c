da1289376df93f7e9c360f6996e950bb1d9fd6b8
ZhangGuoDong <zhangguodong@kylinos.cn>
2026-07-09 14:48:32 +0800

    ksmbd: factor out smb2 open name parsing
    Move name conversion, stream-name parsing and veto checks into a helper.
    
    Signed-off-by: ZhangGuoDong <zhangguodong@kylinos.cn>
    Reviewed-by: ChenXiaoSong <chenxiaosong@kylinos.cn>

--- fs/smb/server/smb2pdu.c
+++ fs/smb/server/smb2pdu.c
@@ -3224,6 +3224,58 @@ static int smb2_parse_posix_create_context(struct ksmbd_work *work,
 	return 0;
 }
 
+static int smb2_open_get_name(struct ksmbd_work *work,
+			      struct smb2_create_req *req,
+			      bool posix_ctxt,
+			      char **name,
+			      char **stream_name,
+			      int *s_type)
+{
+	struct ksmbd_share_config *share = work->tcon->share_conf;
+	int rc;
+
+	if (!req->NameLength) {
+		*name = kstrdup("", KSMBD_DEFAULT_GFP);
+		if (!*name)
+			return -ENOMEM;
+		return 0;
+	}
+
+	*name = smb2_get_name((char *)req + le16_to_cpu(req->NameOffset),
+			      le16_to_cpu(req->NameLength),
+			      work->conn->local_nls);
+	if (IS_ERR(*name)) {
+		rc = PTR_ERR(*name);
+		*name = NULL;
+		return rc;
+	}
+
+	ksmbd_debug(SMB, "converted name = %s\n", *name);
+
+	if (!posix_ctxt) {
+		if (strchr(*name, ':')) {
+			if (!test_share_config_flag(share,
+						    KSMBD_SHARE_FLAG_STREAMS))
+				return -EBADF;
+
+			rc = parse_stream_name(*name, stream_name, s_type);
+			if (rc < 0)
+				return rc;
+		}
+
+		rc = ksmbd_validate_filename(*name);
+		if (rc < 0)
+			return rc;
+	}
+
+	if (ksmbd_share_veto_filename(share, *name)) {
+		ksmbd_debug(SMB, "Reject open(), vetoed file: %s\n", *name);
+		return -ENOENT;
+	}
+
+	return 0;
+}
+
 /**
  * smb2_open() - handler for smb file open request
  * @work:	smb work containing request buffer
@@ -3286,48 +3338,10 @@ int smb2_open(struct ksmbd_work *work)
 	if (rc)
 		goto err_out2;
 
-	if (req->NameLength) {
-		name = smb2_get_name((char *)req + le16_to_cpu(req->NameOffset),
-				     le16_to_cpu(req->NameLength),
-				     work->conn->local_nls);
-		if (IS_ERR(name)) {
-			rc = PTR_ERR(name);
-			name = NULL;
-			goto err_out2;
-		}
-
-		ksmbd_debug(SMB, "converted name = %s\n", name);
-
-		if (posix_ctxt == false) {
-			if (strchr(name, ':')) {
-				if (!test_share_config_flag(work->tcon->share_conf,
-							KSMBD_SHARE_FLAG_STREAMS)) {
-					rc = -EBADF;
-					goto err_out2;
-				}
-				rc = parse_stream_name(name, &stream_name, &s_type);
-				if (rc < 0)
-					goto err_out2;
-			}
-
-			rc = ksmbd_validate_filename(name);
-			if (rc < 0)
-				goto err_out2;
-		}
-
-		if (ksmbd_share_veto_filename(share, name)) {
-			rc = -ENOENT;
-			ksmbd_debug(SMB, "Reject open(), vetoed file: %s\n",
-				    name);
-			goto err_out2;
-		}
-	} else {
-		name = kstrdup("", KSMBD_DEFAULT_GFP);
-		if (!name) {
-			rc = -ENOMEM;
-			goto err_out2;
-		}
-	}
+	rc = smb2_open_get_name(work, req, posix_ctxt, &name, &stream_name,
+				&s_type);
+	if (rc)
+		goto err_out2;
 
 	req_op_level = req->RequestedOplockLevel;
 
